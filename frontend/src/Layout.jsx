d6fd726c7ec55a1b18e73c3a2d0cd3c3208d7123
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-10-21 14:43:50 +0530

    footer add

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 // import Footer from "./Components/Footer";
-
+import Footer from "./components/Footer"
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
