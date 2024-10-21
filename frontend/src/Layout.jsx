210bc1c9cd8c83e0437d8b3a0a4fbf1adf1b47bf
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-10-21 14:20:34 +0530

    lll

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./Components/Footer";
+// import Footer from "./Components/Footer";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
